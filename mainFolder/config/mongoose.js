ac7dc96ea3e708def7a9138123dbf903ba967822
Shashank Shekhar Pandey <91602965+Coder-shank@users.noreply.github.com>
2026-05-24 15:01:40 +0530

    Update mongoose.js

--- mainFolder/config/mongoose.js
+++ mainFolder/config/mongoose.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+// const mongoose = require("mongoose");
+// require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URL);
+// mongoose.connect(process.env.MONGO_URL);
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+// db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-db.once("open", function () {
-  console.log("Connected to Database :: MongoDB");
-});
+// db.once("open", function () {
+//   console.log("Connected to Database :: MongoDB");
+// });
 
-module.exports = db;
+// module.exports = db;
