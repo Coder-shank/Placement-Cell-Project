a02e5a4ff950d04c8478ec3acd764467ccab6f87
Shashank Shekhar Pandey <91602965+Coder-shank@users.noreply.github.com>
2026-05-25 11:18:34 +0530

    Update index.js

--- mainFolder/index.js
+++ mainFolder/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true }));
 app.use(cookieParser());
 
 app.set('view engine', 'ejs');
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 app.use(session({
     name: 'placementcell',
     secret: 'placementcellsecret',
