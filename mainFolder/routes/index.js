4df511731240b23764ca34b46638188bdb0246bd
Shashank Shekhar Pandey <91602965+Coder-shank@users.noreply.github.com>
2026-05-25 08:14:40 +0530

    Update index.js

--- mainFolder/routes/index.js
+++ mainFolder/routes/index.js
@@ -14,7 +14,7 @@ router.get('/signUpUser567emp',homeController.signUp) ;
 router.post('/create', homeController.create);
 
 router.get('/',homeController.signIn);
-router.post('/create-session', passport.authenticate('local', { failureRedirect: '/signIn.ejs', failureMessage: true }),
+router.post('/create-session', passport.authenticate('local', { failureRedirect: '/', failureMessage: true }),
 homeController.createSession) ;
 
 // students related
